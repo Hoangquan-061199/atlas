b0e4fd3afa6cec9af9e3736b6f4da44a373e7592
hoangquan-061199 <hoangquan061199@gmail.com>
2022-11-02 00:31:58 +0700

    completed tra cuu

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,11 +12,12 @@ $(() => {
   });
 
   $('.update-animals .slide-ani').owlCarousel({
-    loop: !1,
+    loop: 1,
     margin: 0,
     nav: !1,
     items: 1,
     dots: 1,
+    autoplay: 1,
   });
 
   const resizeImage = (e, s) => {
@@ -28,7 +29,11 @@ $(() => {
   resizeImage('.news .body .news-item .img', 160 / 307);
   resizeImage('.search .banner', 685 / 1366);
   resizeImage(
-    '.content-about .search .banner, .content-contact .search .banner',
+    '.content-rare-species .lists-species .species-item .img',
+    237 / 418
+  );
+  resizeImage(
+    '.content-about .search .banner, .content-contact .search .banner, .content-rare-species .search .banner, .content-search .search .banner, .content-search-details .search .banner',
     436 / 1366
   );
 
@@ -39,4 +44,48 @@ $(() => {
   if ($(window).width() < 768) {
     // resizeImage('section.map', 385 / 1366);
   }
+});
+$ = jQuery;
+$(document).ready(function () {
+  $v_slider_options = {
+    gallery: true,
+    item: 1,
+    loop: true,
+    slideMargin: 0,
+    thumbItem: 3,
+    galleryMargin: 18,
+    thumbMargin: 18,
+    vertical: true,
+    verticalHeight: 326,
+    vThumbWidth: 173,
+    controls: false,
+  };
+
+  v_slider = $('#lightSliderVertical').lightSlider($v_slider_options);
+
+  /* Fancybox & lightSlider Sync - Bug Fix */
+  $selector = '#lightSliderVertical li:not(".clone") a';
+  $().fancybox({
+    selector: $selector,
+    backFocus: false, //The most important options for sync bug fix
+    buttons: [
+      'slideShow',
+      'share',
+      'zoom',
+      'fullScreen',
+      'thumbs',
+      'download',
+      'close',
+    ],
+  });
+
+  $('.img-orther').fancybox({});
+});
+
+// #RESIZE BUG FIXING
+// # if slider have height in % or vh,
+// # on resize rebuild
+$(window).resize(function () {
+  slider.destroy();
+  h_slider = $('#ocassions-slider').lightSlider(h_slider_options);
 });
