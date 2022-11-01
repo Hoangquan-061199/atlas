54db4d08546f6aa1604e9982719ddec859de3d77
hoangquan-961199 <hoangquan061199@gmail.com>
2022-11-01 17:58:34 +0700

    completed gioi thieu

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,33 @@ $(() => {
     $('.touch-menu').removeClass('active');
     $('.over').removeClass('show');
   });
+
+  $('.update-animals .slide-ani').owlCarousel({
+    loop: !1,
+    margin: 0,
+    nav: !1,
+    items: 1,
+    dots: 1,
+  });
+
+  const resizeImage = (e, s) => {
+    s = parseInt($(e).width()) * s;
+    $(e).css({ height: s + 'px' });
+  };
+
+  resizeImage('.animals .body .ani-item .img', 226 / 420);
+  resizeImage('.news .body .news-item .img', 160 / 307);
+  resizeImage('.search .banner', 685 / 1366);
+  resizeImage(
+    '.content-about .search .banner, .content-contact .search .banner',
+    436 / 1366
+  );
+
+  if ($(window).width() >= 768) {
+    resizeImage('.slide-ani.owl-carousel .owl-item .img', 340 / 650);
+  }
+
+  if ($(window).width() < 768) {
+    // resizeImage('section.map', 385 / 1366);
+  }
 });
