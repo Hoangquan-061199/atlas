b19830346f001681b2e4d921b529cf310593c97d
hoangquan-961199 <hoangquan061199@gmail.com>
2022-11-02 09:14:45 +0700

    completed tin tuc

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,14 +26,11 @@ $(() => {
   };
 
   resizeImage('.animals .body .ani-item .img', 226 / 420);
-  resizeImage('.news .body .news-item .img', 160 / 307);
+  resizeImage('.body .news-item .img', 160 / 307);
   resizeImage('.search .banner', 685 / 1366);
+  resizeImage('.species-item .img', 237 / 418);
   resizeImage(
-    '.content-rare-species .lists-species .species-item .img',
-    237 / 418
-  );
-  resizeImage(
-    '.content-about .search .banner, .content-contact .search .banner, .content-rare-species .search .banner, .content-search .search .banner, .content-search-details .search .banner',
+    '.content-about .search .banner, .content-contact .search .banner, .content-rare-species .search .banner, .content-search .search .banner, .content-search-details .search .banner, .content-news .search .banner',
     436 / 1366
   );
 
@@ -45,47 +42,50 @@ $(() => {
     // resizeImage('section.map', 385 / 1366);
   }
 });
-$ = jQuery;
-$(document).ready(function () {
-  $v_slider_options = {
-    gallery: true,
-    item: 1,
-    loop: true,
-    slideMargin: 0,
-    thumbItem: 3,
-    galleryMargin: 18,
-    thumbMargin: 18,
-    vertical: true,
-    verticalHeight: 326,
-    vThumbWidth: 173,
-    controls: false,
-  };
 
-  v_slider = $('#lightSliderVertical').lightSlider($v_slider_options);
+if ($('.animals-details .slide-ani').length > 0) {
+  $ = jQuery;
+  $(document).ready(function () {
+    $v_slider_options = {
+      gallery: true,
+      item: 1,
+      loop: true,
+      slideMargin: 0,
+      thumbItem: 3,
+      galleryMargin: 18,
+      thumbMargin: 18,
+      vertical: true,
+      verticalHeight: 326,
+      vThumbWidth: 173,
+      controls: false,
+    };
 
-  /* Fancybox & lightSlider Sync - Bug Fix */
-  $selector = '#lightSliderVertical li:not(".clone") a';
-  $().fancybox({
-    selector: $selector,
-    backFocus: false, //The most important options for sync bug fix
-    buttons: [
-      'slideShow',
-      'share',
-      'zoom',
-      'fullScreen',
-      'thumbs',
-      'download',
-      'close',
-    ],
+    v_slider = $('#lightSliderVertical').lightSlider($v_slider_options);
+
+    /* Fancybox & lightSlider Sync - Bug Fix */
+    $selector = '#lightSliderVertical li:not(".clone") a';
+    $().fancybox({
+      selector: $selector,
+      backFocus: false, //The most important options for sync bug fix
+      buttons: [
+        'slideShow',
+        'share',
+        'zoom',
+        'fullScreen',
+        'thumbs',
+        'download',
+        'close',
+      ],
+    });
+
+    $('.img-orther').fancybox({});
   });
 
-  $('.img-orther').fancybox({});
-});
-
-// #RESIZE BUG FIXING
-// # if slider have height in % or vh,
-// # on resize rebuild
-$(window).resize(function () {
-  slider.destroy();
-  h_slider = $('#ocassions-slider').lightSlider(h_slider_options);
-});
+  // #RESIZE BUG FIXING
+  // # if slider have height in % or vh,
+  // # on resize rebuild
+  $(window).resize(function () {
+    slider.destroy();
+    h_slider = $('#ocassions-slider').lightSlider(h_slider_options);
+  });
+}
